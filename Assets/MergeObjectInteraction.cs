06b10e9a3f50b3d100d7a1c6a6352841cefb6516
marunemo <chys3697@gmail.com>
2021-05-03 01:06:35 +0900

    Make Interaction Code of Merging Object
    ------------------
    게임 오브젝트간 충돌 상호작용 코드 생성
    ------------------

---
+++ Assets/MergeObjectInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MergeObjectInteraction : MonoBehaviour {
+
+    public GameObject nextMerge;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.name[2].Equals(this.gameObject.name[2])) {
+            if(this.transform.position.y > col.transform.position.y)
+                Instantiate(nextMerge,
+                            new Vector2(0, (col.transform.position.y + this.transform.position.y) / 2),
+                            Quaternion.Euler(0, 0, Random.Range(-360, 360)));
+            Destroy(this.gameObject);
+        }
+    }
+}
