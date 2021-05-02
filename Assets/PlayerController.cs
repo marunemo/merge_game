4141e78249f2cff78f740acb32ffa97a5af2cbce
marunemo <chys3697@gmail.com>
2021-05-03 02:28:30 +0900

    bug fixed
    ----------------
    마우스 위치에 맞게 공이 떨어지도록 수정
    ----------------

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
 
     public GameObject target;
 
+    public Transform lmax, rmax;
+
     // Start is called before the first frame update
     void Start() {
         
@@ -14,8 +16,7 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if(Input.GetMouseButtonDown(0)) {
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-            pos.z = 0;
+            Vector3 pos = new Vector3(Input.mousePosition.x / Screen.width * (rmax.position.x - lmax.position.x) + lmax.position.x, 3, 0);
             Instantiate(target, pos, Quaternion.Euler(0, 0, Random.Range(-360, 360)));
         }
     }
